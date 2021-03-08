72ee413a10578250ebdf41486e06414abcfb34c6
sieun-park <79831433+sieun-park@users.noreply.github.com>
2021-03-08 12:38:01 +0900

    prewiew js

---
+++ script.js
@@ -0,0 +1,10 @@
+const gallery = document.querySelectorAll(".gallery .images"),
+previewBox = document.querySelector(".preview-box");
+
+window.onload = ()=>{
+    for (let i = 0; i < gallery.length; i++) {
+        console.log(i);
+
+        
+    }
+}
