20bdfa553ab0610787996a9abf397bee4ead236d
sieun-park <79831433+sieun-park@users.noreply.github.com>
2021-03-10 15:01:41 +0900

    preview js
    added more function in the preview box

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const gallery = document.querySelectorAll(".gallery .image"),
-previewBox = document.querySelector(".preview-box");
+previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon");
 
 window.onload = ()=>{
     for (let i = 0; i < gallery.length; i++) {
+        let newIndex = i;
         gallery[i].onclick = ()=>{
-            console.log(i);
+            console.log(i); 
+            function preview(){
+                let selectedImgUrl = gallery[i].querySelector("img").src;
+                previewImg.scr = selectedImgUrl;
+                console.log(selectedImgUrl);
+            }
+            preview();
             previewBox.classList.add("show");
+            closeIcon.onclick = ()=>{
+                previewBox.classList.remove("show");
+
+            }
+        
         }
     }
 }
