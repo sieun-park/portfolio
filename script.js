060078a5a5f8a0dd46a9bf76d0513f80a2ed1118
sieun-park <79831433+sieun-park@users.noreply.github.com>
2021-03-19 13:11:12 +0900

    finish navbar

--- script.js
+++ script.js
@@ -70,6 +70,7 @@ window.onload = ()=>{
 
 const navbarToggler = document.querySelector(".navbar-toggler");
 const navbarMenu = document.querySelector(".navbar ul");
+const navbarLinks = document.querySelectorAll(".navbar a");
 
 navbarToggler.addEventListener("click", navbarTogglerClick);
 
@@ -77,4 +78,11 @@ function navbarTogglerClick(){
     navbarToggler.classList.toggle("open-navber-toggler");
     navbarMenu.classList.toggle("open");
 }
-  
+
+navbarLinks.forEach(elem => elem.addEventListener("click",navbarLinkClick));
+ 
+function navbarLinkClick() {
+    if(navbarMenu.classList.contains("open")) {
+        navbarToggler.click();
+    }
+}
